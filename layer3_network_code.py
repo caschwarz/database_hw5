c5ff4bebdb1922f3e5b617110e5751e165cfdbf6
caschwarz <clarkaschwarz@outlook.com>
2025-04-24 13:09:28 -0400

    added changes for part 3

--- layer3_network_code.py
+++ layer3_network_code.py
@@ -1,3 +1,8 @@
+"""
+This is a network simulator that simulates 3 routes with 2 hosts connected to each of them
+"""
+
+# imports
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import Node, OVSKernelSwitch
@@ -6,7 +11,6 @@ from mininet.cli import CLI
 from mininet.log import setLogLevel
 
 class LinuxRouter(Node):
-    """A Node with IP forwarding enabled."""
     def config(self, **params):
         super(LinuxRouter, self).config(**params)
         self.cmd('sysctl net.ipv4.ip_forward=1')
@@ -46,18 +50,53 @@ class CustomTopo(Topo):
         self.addLink(routerB, routerC, intfName1='rB-eth4', intfName2='rC-eth3',
                      params1={'ip': '20.10.100.4/24'}, params2={'ip': '20.10.100.3/24'})
 
+"""
+This function runs the main part of the program. It begins by creating host and routers and defining the links
+between them. Then, it displays data on the connectivity info.
+"""
 def run():
     topo = CustomTopo()
     net = Mininet(topo=topo, switch=OVSKernelSwitch, controller=None, link=TCLink, autoSetMacs=True)
     net.start()
 
-    print("\n===== Testing Intra-LAN Connectivity =====")
-    net.ping([net.get('hA1'), net.get('hA2')])
-    net.ping([net.get('hB1'), net.get('hB2')])
-    net.ping([net.get('hC1'), net.get('hC2')])
+    hA1, hA2 = net.get('hA1'), net.get('hA2')
+    hB1, hB2 = net.get('hB1'), net.get('hB2')
+    hC1, hC2 = net.get('hC1'), net.get('hC2')
+    rA, rB, rC = net.get('rA'), net.get('rB'), net.get('rC')
 
-    print("\n===== Inter-LAN Ping (Should Fail) =====")
+    # Add static routes on hosts
+    for h in [hA1, hA2]:
+        h.cmd('route add -net 20.10.172.0 netmask 255.255.255.128 gw 20.10.172.129')
+        h.cmd('route add -net 20.10.172.192 netmask 255.255.255.224 gw 20.10.172.129')
+
+    for h in [hB1, hB2]:
+        h.cmd('route add -net 20.10.172.128 netmask 255.255.255.192 gw 20.10.172.1')
+        h.cmd('route add -net 20.10.172.192 netmask 255.255.255.224 gw 20.10.172.1')
+
+    for h in [hC1, hC2]:
+        h.cmd('route add -net 20.10.172.0 netmask 255.255.255.128 gw 20.10.172.193')
+        h.cmd('route add -net 20.10.172.128 netmask 255.255.255.192 gw 20.10.172.193')
+
+    # Add static routes on routers
+    rA.cmd('route add -net 20.10.172.0 netmask 255.255.255.128 gw 20.10.100.2')
+    rA.cmd('route add -net 20.10.172.192 netmask 255.255.255.224 gw 20.10.100.2')
+
+    rB.cmd('route add -net 20.10.172.128 netmask 255.255.255.192 gw 20.10.100.1')
+    rB.cmd('route add -net 20.10.172.192 netmask 255.255.255.224 gw 20.10.100.3')
+
+    rC.cmd('route add -net 20.10.172.128 netmask 255.255.255.192 gw 20.10.100.2')
+    rC.cmd('route add -net 20.10.172.0 netmask 255.255.255.128 gw 20.10.100.2')
+
+    print("\n===== Testing Intra-LAN Connectivity =====")
+    net.ping([hA1, hA2])
+    net.ping([hB1, hB2])
+    net.ping([hC1, hC2])
+
+    print("\n===== Testing Inter-LAN Connectivity =====")
     net.pingAll()
+
+    print("\n===== Traceroute from hA1 to hC1 =====")
+    print(hA1.cmd('traceroute 20.10.172.193'))
 
     CLI(net)
     net.stop()
